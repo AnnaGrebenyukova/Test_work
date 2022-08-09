bfb19ac286e1bdb08aa03dd7dcf17a0fd81986e7
Anna <Annagrex@rambler.ru>
2022-08-09 21:55:29 +0300

    add Task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ShowOldArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] +" ");
+    Console.WriteLine();
+}
+
+void FindThree(string[] array, string[] newarray)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        newarray[count] = array[i];
+        count++;
+    }
+}
+void ShowNewArray(string[] newarray)
+{
+    for(int i = 0; i < newarray.Length; i++)
+        Console.Write(newarray[i] +" ");
+}
+
+string[] array = new string[] { "Sun", "Paris", "22", "0", "Hello", "++", "world" };
+ShowOldArray(array);
+string[] newArray = new string[array.Length];
+FindThree(array, newArray);
+ShowNewArray(newArray);
